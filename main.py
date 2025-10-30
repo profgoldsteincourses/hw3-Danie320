946215e33fd90146d0b5887c4c6c5da10c3841ad
Danie320 <dj20mill@siena.edu>
2025-10-30 10:27:22 -0400

    Finished product

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import random
 
 # makeID takes two ...
 # and returns...
-def makeID(     ,     ):
+def makeID(first, last):
     # Extract the first inital of their first name
+
+    first_name = first[0]
 
     # Extract the first five ...
 
+    last_name = last[0:5]
+
     # Get a random number between 10 and 99
-    myNumber = random.range(   ,   )
+    myNumber = random.randrange(10, 100)
 
     # Put it all together (concatenate)
+
+    ID = first_name + last_name + str(myNumber)
 
     return ID
 
@@ -20,16 +26,18 @@ def makeID(     ,     ):
 # main prompts the user ....
 def main():
     # Ask for their first name
+
     firstName = input("What is your first name? ")
 
     # Ask for their last name
 
+    lastName = input("What is your last name?: ")
 
     # Call makeID
-    newID =
+    newID = makeID(firstName, lastName)
 
     # Print out the newID
-
+    print(newID)
 
 
 # Function definitions above
